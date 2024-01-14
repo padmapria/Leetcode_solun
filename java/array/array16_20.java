5056ab22c797f0bc96b88deec5630886fd01f6d7
padmapria <deepika3.deepika3@gmail.com>
2024-01-14 23:29:24 +0800

    19 and 20

--- java/array/array16_20.java
+++ java/array/array16_20.java
@@ -37,7 +37,7 @@ class Solution {
     }
 }
 /*
-You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
+17. You are given a 2D integer array logs where each logs[i] = [birthi, deathi] indicates the birth and death years of the ith person.
 
 The population of some year x is the number of people alive during that year. The ith person is counted in year x's population if x is in the inclusive range [birthi, deathi - 1]. Note that the person is not counted in the year that they die.
 
@@ -83,3 +83,72 @@ class Solution {
     }
 }
 
+/*
+19. Two sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+*/
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] index_arr = new int[2];
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            
+            int diff =  target-nums[i];
+
+            if (hm.containsKey(diff)) {
+                index_arr[0]=  hm.get(diff);
+                index_arr[1]  = i;
+            }
+            hm.putIfAbsent(nums[i],i);
+        }
+        return index_arr;
+        
+    }
+}
+
+
+/*
+20. Find N Unique Integers Sum up to Zero
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+Example 1:
+
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+Example 2:
+*/
+class Solution {
+    public int[] sumZero(int n) {
+        int [ ]arr = new int[n];
+
+        if(n==1){
+           arr[0] = 0;
+        }
+
+          if(n%2!=0){
+            arr[n/2]= 0;
+        }
+        
+        for(int i=0;i<(n/2);i++){
+            arr[i] = i+1;
+            arr[n-i-1]= -(arr[i]);
+        }
+
+        return arr;
+    }
+}
