ba08f405846ebbd74a5bcd9f7b0fc456ed53f09b
padmapria <deepika3.deepika3@gmail.com>
2024-01-15 23:15:22 +0800

    added majority element

--- java/ly_homework/loop.java
+++ java/ly_homework/loop.java
@@ -78,3 +78,47 @@ class Solution {
         return is_power_ofTwo;
     }
 }
+
+/*
+Majority Element
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+*/
+import java.util.HashMap;
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        int rep_no = 0;
+        int max_count =0;
+        for(int i=0;i<nums.length;i++){
+            
+            int value =  1;
+
+            if (hm.containsKey(nums[i])) {
+                value+= hm.get(nums[i]);
+                hm.put(nums[i], value);
+            } else {
+                hm.put(nums[i], value);  
+            }
+
+
+            if(max_count<value){
+                max_count= value;
+                rep_no = nums[i];
+            }
+        }
+        return rep_no;
+    }
+}
